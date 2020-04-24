a3d11e5d26827b31dc5d87b9eb94fdc38b4f887d
Fidel Jose <fideljose1708@hotmail.com>
2020-04-24 17:58:41 -0500

    Add Query Param Optional value - Rest Controlles

--- src/main/java/com/fideljose/reactivemiscelanea/controller/PersonaController.java
+++ src/main/java/com/fideljose/reactivemiscelanea/controller/PersonaController.java
@@ -1,5 +1,7 @@
 package com.fideljose.reactivemiscelanea.controller;
 
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonaController {
 
 	@GetMapping({"index", "/"})
-	public String index(@RequestParam String id, String name) {
+	public String index(@RequestParam(required=false) String id, String name) {
 		return "Respuesta" + " => ID " + id + " Name " + name + "!!!";
 	}
 	
