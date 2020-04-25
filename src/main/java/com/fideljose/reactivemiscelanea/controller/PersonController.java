3e666fd1c78c1da233622bd7f55e640c7f8aac9d
Fidel Jose <fideljose1708@hotmail.com>
2020-04-24 19:20:51 -0500

    Add config to create xml request and response - Rest Endpo+int

--- src/main/java/com/fideljose/reactivemiscelanea/controller/PersonController.java
+++ src/main/java/com/fideljose/reactivemiscelanea/controller/PersonController.java
@@ -1,19 +1,27 @@
 package com.fideljose.reactivemiscelanea.controller;
 
-import java.util.Optional;
-
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fideljose.reactivemiscelanea.Person;
+
 @RestController()
 @RequestMapping("/api")
-public class PersonaController {
+public class PersonController {
 
-	@GetMapping({"index", "/"})
+	@GetMapping(value={"index", "/"})
 	public String index(@RequestParam(required=false) String id, String name) {
 		return "Respuesta" + " => ID " + id + " Name " + name + "!!!";
+	}
+	
+	@GetMapping(value={"responsexml"})//,
+//			consumes = MediaType.APPLICATION_JSON_VALUE,
+//			produces = MediaType.APPLICATION_XML_VALUE)
+	public Person responseXML(@RequestParam(required=false) String id, String name) {
+		return new Person("fidel", "Villero", "fidel@fidel.com", "COLOMBIA");
 	}
 	
 }
